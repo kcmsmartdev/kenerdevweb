313a30a864cec020253b5c480f15ee1257ee5d8d
Kener Cotrina <kenercotrin@gmail.com>
2026-01-21 02:19:38 -0500

    se agrega menu movil

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import tailwindcss from "@tailwindcss/vite";
+import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
@@ -9,4 +10,5 @@ export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
+  integrations: [icon()],
 });
